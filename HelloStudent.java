566bb86c6cd72b6961277eb1feedc98ad54fe8d9
rushikesh <rushikesh123@gmail.com>
2025-05-08 02:38:02 +0530

    java file

--- HelloStudent.java
+++ HelloStudent.java
@@ -1,5 +1,7 @@
 public class HelloStudent {
     public static void main(String[] args) {
         System.out.println("Hello, My Name is John Doe");
+	System.out.println("Hello, Welcome to Cdac");
+
     }
 }
